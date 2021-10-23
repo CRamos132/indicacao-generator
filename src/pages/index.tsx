95117c21794ece51f1495767f20038574d609512
CRamos132 <camille.gpramos@gmail.com>
2021-10-23 11:48:13 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,76 @@
+import type { NextPage } from "next";
+import { Flex, Box, Text, Input, Select, Button } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import generator from "../utils/generator";
+import { useState } from "react";
+import copyText from "../utils/copy";
+
+const Home: NextPage = () => {
+  const { register, handleSubmit } = useForm();
+  const [indicacoes, setIndicacoes] = useState<string[]>([]);
+  const onSubmit = ({ nome, projeto }: Record<string, string>) => {
+    const indicacao = generator({ nome, projeto });
+    setIndicacoes([indicacao]);
+    console.info(indicacao);
+  };
+  return (
+    <Flex
+      direction="column"
+      alignItems="center"
+      padding="32px"
+      gridRowGap="24px"
+    >
+      <Text as="h1" fontSize="2rem" fontWeight="bold">
+        Indicação generator
+      </Text>
+      <Text as="h2" fontSize="1.2rem">
+        Trabalhadores do mundo uni-vos
+      </Text>
+      <Flex
+        as="form"
+        onSubmit={handleSubmit(onSubmit)}
+        direction="column"
+        alignItems="center"
+        width="30%"
+        minWidth="300px"
+        gridRowGap="24px"
+      >
+        <Box as="label" width="100%">
+          Nome da pessoa*
+          <Input
+            type="text"
+            placeholder="Nome do slack"
+            {...register("nome", { required: true })}
+          />
+        </Box>
+        <Box as="label" width="100%">
+          Projeto/Setor
+          <Input type="text" placeholder="Projeto" {...register("projeto")} />
+        </Box>
+        <Button type="submit">Gerar indicações</Button>
+      </Flex>
+      {indicacoes.map((indicacao) => (
+        <Flex
+          direction="row"
+          alignItems="center"
+          gridColumnGap="8px"
+          borderRadius="8px"
+          backgroundColor="gray.100"
+          padding="12px"
+        >
+          <Text>{indicacao}</Text>
+          <Button
+            type="button"
+            onClick={() => {
+              copyText(indicacao);
+            }}
+          >
+            Copiar
+          </Button>
+        </Flex>
+      ))}
+    </Flex>
+  );
+};
+
+export default Home;
---
+++ src/utils/generator.ts
@@ -0,0 +1,35 @@
+interface IParams {
+  nome: string;
+  projeto?: string;
+}
+
+const intros = ["Queria indicar", "Vou indicar", "Indicando"];
+const comProjeto = ["seu trampo foda em", "arregaçar em", "DETONAR"];
+const semProjeto = [
+  "amassar os gringo",
+  "ser foda",
+  "sempre estar ajudando a galera",
+];
+
+const getFromArray = (array: string[]) => {
+  const selected = Math.floor(Math.random() * array.length);
+  return array[selected];
+};
+
+export default function generator({ nome, projeto }: IParams) {
+  let indicacao = "";
+  const hasIntro = Math.floor(Math.random() * 10) % 2 === 0;
+  if (hasIntro) {
+    const intro = getFromArray(intros);
+    indicacao += intro;
+  }
+  indicacao += ` @${nome}`;
+  if (projeto) {
+    const motivo = getFromArray(comProjeto);
+    indicacao += ` por ${motivo} ${projeto}`;
+  } else {
+    const motivo = getFromArray(semProjeto);
+    indicacao += ` por ${motivo} ${projeto}`;
+  }
+  return indicacao;
+}
